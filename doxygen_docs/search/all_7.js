8895e5baa5bf34289d02d3c7966a3e36d503e8b3
Dan Gordon <dan.gordon@nicta.com.au>
2015-06-16 13:41:01 +1000

    Working on docs.

--- doxygen_docs/search/all_7.js
+++ doxygen_docs/search/all_7.js
@@ -2,7 +2,9 @@ var searchData=
 [
   ['hasneutral_5f',['hasNeutral_',['../class_sgt_1_1_underground_line.html#a0c842c213fa141317de2f3d8c9476918',1,'Sgt::UndergroundLine']]],
   ['hasproperties',['HasProperties',['../class_sgt_1_1_has_properties.html',1,'Sgt']]],
+  ['hasproperties_3c_20bar_20_3e',['HasProperties&lt; Bar &gt;',['../class_sgt_1_1_has_properties.html',1,'Sgt']]],
   ['hasproperties_3c_20bus_20_3e',['HasProperties&lt; Bus &gt;',['../class_sgt_1_1_has_properties.html',1,'Sgt']]],
+  ['hasproperties_3c_20foo_20_3e',['HasProperties&lt; Foo &gt;',['../class_sgt_1_1_has_properties.html',1,'Sgt']]],
   ['haspropertiesinterface',['HasPropertiesInterface',['../class_sgt_1_1_has_properties_interface.html',1,'Sgt']]],
   ['heartbeat',['Heartbeat',['../class_sgt_1_1_heartbeat.html',1,'Sgt']]],
   ['heartbeatadaptor',['HeartbeatAdaptor',['../class_sgt_1_1_heartbeat_adaptor.html',1,'Sgt']]],
