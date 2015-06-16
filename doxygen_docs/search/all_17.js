9e5f5b52d404011eaa9cb7c27f5de9bc662c4e35
Dan Gordon <dan.gordon@nicta.com.au>
2015-06-16 10:53:07 +1000

    Add new docs files.

---
+++ doxygen_docs/search/all_17.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['zenith',['zenith',['../struct_sgt_1_1_spherical_angles.html#af6659d1fdc1a548f2d7d7b488fea1d98',1,'Sgt::SphericalAngles']]],
+  ['zipabc',['ZipAbc',['../class_sgt_1_1_zip_abc.html',1,'Sgt']]],
+  ['zline2ynode',['ZLine2YNode',['../group___power_flow_core.html#gac77aa4c2855649f65d6642d4bbdfd8ae',1,'Sgt']]],
+  ['zphase',['ZPhase',['../class_sgt_1_1_overhead_line.html#a81a6bb2e9afa8e225f7a44c8605ba5cb',1,'Sgt::OverheadLine::ZPhase()'],['../class_sgt_1_1_underground_line.html#a4c20bc6d02954fbc45cf6a4d5551b1b0',1,'Sgt::UndergroundLine::ZPhase()']]],
+  ['zphase_5f',['ZPhase_',['../class_sgt_1_1_underground_line.html#a0e930e51b78caa0262dd8c7476fffb77',1,'Sgt::UndergroundLine']]],
+  ['zprim',['ZPrim',['../class_sgt_1_1_overhead_line.html#ae9121105ad42178fef0e75f590a6daeb',1,'Sgt::OverheadLine::ZPrim()'],['../class_sgt_1_1_underground_line.html#a7c435d3b7bfaebbd6022604349ff4cb9',1,'Sgt::UndergroundLine::ZPrim()']]],
+  ['zprim_5f',['ZPrim_',['../class_sgt_1_1_underground_line.html#ab875cf88330ccd5696fc2b0c07c1cece',1,'Sgt::UndergroundLine']]]
+];
---
+++ doxygen_docs/search/functions_13.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['willstartnewtimestep',['willStartNewTimestep',['../class_sgt_1_1_sim_component_adaptor.html#ad48d08af91904e165335d6749995b060',1,'Sgt::SimComponentAdaptor']]],
+  ['willupdate',['willUpdate',['../class_sgt_1_1_sim_component_adaptor.html#a3e21ef7f8e14b4526711ff152047994a',1,'Sgt::SimComponentAdaptor']]]
+];
