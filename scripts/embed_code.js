55049f742a8a3577a9b1b76fa34e7fd7c68b5fa7
Dan Gordon <dan.gordon@nicta.com.au>
2015-06-18 11:32:26 +1000

    Work on code embedding.

--- scripts/embed_code.js
+++ scripts/embed_code.js
@@ -1,11 +1,24 @@
+var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+};
+
+function escapeHtml(string) {
+    return String(string).replace(/[&<>"'\/]/g, function(s)
+                                  {return entityMap[s];});
+}
+
+
 var code = document.getElementsByClassName("code")
 var pre;
 for (var i = 0; i < code.length; i++) {
 	pre=document.createElement("pre");
-	// pre.classList.add("prettyprint");
-	// pre.classList.add("lang-cpp");
 	pre.innerHTML = '<code class="prettyprint lang-cpp">'
-				  + code[i].innerHTML.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;") 
+				  + escapeHtml(code[i].innerHTML)
 				  + '</code>';
 	code[i].parentNode.insertBefore(pre,code[i]);
 }
