6f1d70bc69912cb421df31843a61808de78b6fcf
Dan Gordon <dan.gordon@nicta.com.au>
2015-07-02 15:18:10 +1000

    Update doxygen docs.

--- doxygen_docs/search/classes_0.js
+++ doxygen_docs/search/classes_0.js
@@ -1,8 +1,4 @@
 var searchData=
 [
-  ['a',['A',['../classinterfaces_1_1_a.html',1,'interfaces']]],
-  ['a1',['A1',['../classinterfaces_1_1_a1.html',1,'interfaces']]],
-  ['a2',['A2',['../classinterfaces_1_1_a2.html',1,'interfaces']]],
-  ['action',['Action',['../class_sgt_1_1_action.html',1,'Sgt']]],
-  ['ainterface',['AInterface',['../classinterfaces_1_1_a_interface.html',1,'interfaces']]]
+  ['action',['Action',['../class_sgt_1_1_action.html',1,'Sgt']]]
 ];
